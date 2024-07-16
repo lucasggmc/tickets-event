b263fa1ca5600d98e1b1defe810d3932abbaef4e
lucasggmc <lucasggmc@gmail.com>
2024-07-16 19:02:49 -0300

    added MUI pagination component and logic on list component

--- src/app/features/list/list.component.ts
+++ src/app/features/list/list.component.ts
@@ -1,10 +1,11 @@
 import { Component, inject } from '@angular/core';
 import { EventsService } from '../../shared/services/events.service';
+import { MatPaginatorModule, PageEvent } from '@angular/material/paginator';
 
 @Component({
   selector: 'app-list',
   standalone: true,
-  imports: [],
+  imports: [MatPaginatorModule],
   templateUrl: './list.component.html',
   styleUrl: './list.component.scss'
 })
@@ -12,15 +13,24 @@ export class ListComponent {
   eventService = inject(EventsService);
   events?: any
 
+  totalItems: number = 0;
+  pageSize: number = 20;
+  currentPage: number = 0;
 
   ngOnInit() {
     this.getEvents(0);
   }
 
+  pageChanged(event: PageEvent) {
+    this.currentPage = event.pageIndex;
+    this.getEvents(event.pageIndex);
+  }
+
   getEvents(page?: number, searchCity?: string, startDate?: Date, endDate?: Date) {
-    this.eventService.getAll(page || 0, searchCity, startDate, endDate).subscribe((data) => {
-      const { _embedded } = data;
+    this.eventService.getAll(page || this.currentPage, searchCity, startDate, endDate).subscribe((data) => {
+      const { _embedded, page: { totalPages } } = data;
       this.events = _embedded.events;
+      this.totalItems = totalPages;
     });
   }
 
